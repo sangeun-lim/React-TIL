09e455bb3b7d69e4cf8f966840a7817003de6fb7
sangeun <lse2625@gmail.com>
2022-06-26 15:25:19 +0900

    0626

---
+++ dicegame/src/index.js
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom/client';
+
+const product = 'MacBook';
+const model = ' Air';
+const item = product + model;
+
+function handleClick() {
+  alert('곧 도착!');
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <h1>나만의 {item} 주문하기</h1>
+    <button onClick={handleClick}>확인</button>
+  </>,
+);
